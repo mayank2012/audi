af3eda3267050cec1e591746a44c3e3a3ed88aee
Devesh Kumar <deveshkumarsep12@outlook.com>
2021-04-06 12:06:40 +0530

    code fixed

--- routes/githubjobs.js
+++ routes/githubjobs.js
@@ -1,26 +1,23 @@
 import express from "express";
 import axios from "axios";
-
 const router = express.Router();
 
 /**
  * @openapi
- * /api/githubJobs/fetch
+ * /api/githubJobs/fetch:
  *   get:
  *     tags:
  *       - Github
  *     security:
  *       - bearerAuth: []
- *     summary: List all the posts
+ *     summary: List all github listed jobs
  *     responses:
  *       '200':
  *         description: Able to retrive all the posts
- *       '401':
- *         description: Please pass the correct User Access Token
+ *       '400':
+ *         description: Unable to parse the data
  */
-router.get("/fetch", verify, async (req, res) => {
-    var restResonse = [];
-
+router.get("/fetch", async (req, res) => {
     var config = {
         method: "get",
         url: "https://jobs.github.com/positions.json",
@@ -31,20 +28,12 @@ router.get("/fetch", verify, async (req, res) => {
 
     axios(config)
         .then(function (response) {
-            restResponse = JSON.stringify(response.data);
+            // console.log(JSON.stringify(response.data));
+            res.send(response.data);
         })
         .catch(function (error) {
-            console.log(error);
+            res.sendStatus(400).send(error);
         });
-
-    // res.json({
-    //     status: {
-    //         errorCode: "0",
-    //         message: "Successfully up and Running.",
-    //         discription:
-    //             "Go on for the API Documentation mentioned at https://explore.postman.com/templates/13195/smauthserver  or reach at <Host-Url>/api-docs/",
-    //     },
-    // });
 });
 
 export default router;
